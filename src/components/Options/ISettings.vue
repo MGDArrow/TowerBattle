0ba0a47b60f97497d1f490e01d6bd52abc2efc83
MGDArrow <PSMuz@mgdmail.ru>
2024-07-16 21:31:29 +0300

    feat(components): заполнить заглушки Магазина, Правил и Настроек

--- src/components/Options/ISettings.vue
+++ src/components/Options/ISettings.vue
@@ -1,14 +1,75 @@
 <template>
   <div class="options__settings frame-blue shadow-h-blue bg-bg">
     <h1 class="bg-blue">Настройки</h1>
+    <div class="options__settings-body settings">
+      <div class="settings__point">
+        <div class="settings__name color-blue">FPS:</div>
+        <div class="settings__set">
+          <span class="border-blue" :class="{ setting__act: Settings.fps.value === 30 }" @click.stop="setNewFPS(30)"
+            >30</span
+          >
+          <span class="border-blue" :class="{ setting__act: Settings.fps.value === 60 }" @click.stop="setNewFPS(60)"
+            >60</span
+          >
+          <span class="border-blue" :class="{ setting__act: Settings.fps.value === 120 }" @click.stop="setNewFPS(120)"
+            >120</span
+          >
+          <span class="border-blue" :class="{ setting__act: Settings.fps.value === 144 }" @click.stop="setNewFPS(144)"
+            >144</span
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import Settings from '@/logic/settings';
+
+  function setNewFPS(newFPS: number): void {
+    Settings.fps.value = newFPS;
+  }
+</script>
 
 <style lang="scss">
   .options__settings {
+    display: flex;
+    flex-direction: column;
     width: 100%;
     height: 100%;
+    &-body {
+      flex: 1;
+      padding: 1dvh;
+    }
+  }
+  .settings {
+    &__point {
+      display: flex;
+      width: 100%;
+      margin: 0.8dvh 0;
+      font-size: 1.5rem;
+    }
+    &__name {
+      width: 30%;
+      text-align: right;
+    }
+    &__set {
+      display: flex;
+      gap: 1dvh;
+      justify-content: space-between;
+      width: 70%;
+      padding: 0 1dvh;
+      cursor: pointer;
+      & span {
+        flex: 1;
+        text-align: center;
+        transition: $transition-main;
+        &:hover,
+        &.setting__act {
+          background: $col-blue;
+          box-shadow: $size-shadow $col-blue;
+        }
+      }
+    }
   }
 </style>
--- src/components/Options/IShop.vue
+++ src/components/Options/IShop.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="options__shop frame-yellow shadow-h-yellow bg-bg">
     <h1 class="bg-yellow">Магазин</h1>
+    <div class="options__shop-body shop scroll-yellow">
+      <div class="shop__point border-yellow bg-bg bg-h-yellow shadow-h-yellow">
+        <div class="shop__count">100</div>
+        <div class="shop__icon flex-center"><VIcon :name="'diamond'" /></div>
+        <div class="shop__price">200 рублей</div>
+      </div>
+      <div class="shop__point border-yellow bg-bg bg-h-yellow shadow-h-yellow">
+        <div class="shop__count">250</div>
+        <div class="shop__icon flex-center"><VIcon :name="'diamond'" /></div>
+        <div class="shop__price">400 рублей</div>
+      </div>
+      <div class="shop__point border-yellow bg-bg bg-h-yellow shadow-h-yellow">
+        <div class="shop__count">500</div>
+        <div class="shop__icon flex-center"><VIcon :name="'diamond'" /></div>
+        <div class="shop__price">700 рублей</div>
+      </div>
+      <div class="shop__point border-yellow bg-bg bg-h-yellow shadow-h-yellow">
+        <div class="shop__count">1000</div>
+        <div class="shop__icon flex-center"><VIcon :name="'diamond'" /></div>
+        <div class="shop__price">1200 рублей</div>
+      </div>
+      <div class="shop__point border-yellow bg-bg bg-h-yellow shadow-h-yellow">
+        <div class="shop__count">2500</div>
+        <div class="shop__icon flex-center"><VIcon :name="'diamond'" /></div>
+        <div class="shop__price">2700 рублей</div>
+      </div>
+      <div class="shop__point border-yellow bg-bg bg-h-yellow shadow-h-yellow">
+        <div class="shop__count">5000</div>
+        <div class="shop__icon flex-center"><VIcon :name="'diamond'" /></div>
+        <div class="shop__price">5000 рублей</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,7 +40,40 @@
 
 <style lang="scss">
   .options__shop {
+    display: flex;
+    flex-direction: column;
     width: 100%;
     height: 100%;
+    &-body {
+      flex: 1;
+      padding: 1dvh;
+    }
+  }
+  .shop {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1dvh;
+    align-content: start;
+    &__point {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: calc(100% / 3 - (2dvh / 3));
+      height: 35dvh;
+      padding: 0.5dvh;
+      cursor: pointer;
+    }
+    &__count {
+      font-size: 4rem;
+      text-align: center;
+    }
+    &__icon {
+      font-size: 8rem;
+      text-align: center;
+    }
+    &__price {
+      font-size: 1.5rem;
+      text-align: center;
+    }
   }
 </style>
