91af55b204877767dd521f6008c426d9c6c033c4
MGDArrow <PSMuz@mgdmail.ru>
2024-07-22 16:56:31 +0300

    fix(components): изменить положение подсказок в игре

--- src/components/Game/Panel/PanelDescription.vue
+++ src/components/Game/Panel/PanelDescription.vue
@@ -1,16 +1,11 @@
 <template>
-  <div
-    class="game__description bg-bg border-turq flex-center"
-    v-if="isDescription"
-    :style="{ height: descriptionHeight }"
-  >
+  <div class="game__description bg-bg border-turq flex-center" v-if="isDescription">
     {{ descriptionText }}
   </div>
 </template>
 
 <script setup lang="ts">
   import { computed, ComputedRef } from 'vue';
-  import Updates from '@/mechanics/updates';
   import { IUpdate } from '@/types/updates';
 
   interface Props {
@@ -19,10 +14,6 @@
   const props = defineProps<Props>();
 
   const isDescription = computed(() => props.description !== '');
-
-  const descriptionHeight = computed(() =>
-    Updates.updates['defence'].groups['kit'].updates['percent'].lvl_max > 0 ? '9.3dvh' : '7.9dvh',
-  );
 
   const descriptionText = computed(() => {
     if (props.description?.description) return props.description?.description;
@@ -33,10 +24,11 @@
 <style lang="scss">
   .game__description {
     position: fixed;
-    top: 5.7dvh;
+    bottom: 16dvh;
     left: 50%;
-    width: calc(50% - 5.5dvh);
-    height: 9.3dvh;
+    max-width: calc(50% - 5.5dvh);
+    min-height: 7.9dvh;
+    max-height: 9.3dvh;
     padding: 0 5dvh;
     text-align: center;
     transform: translateX(-50%);
